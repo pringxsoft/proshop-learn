ff714f6d5c89cbc53098f36f1b5a07c6bbf65d3e
PringX-Justin <justin.xing@prolinkchina.com>
2023-11-23 14:57:52 +0800

    fix bug

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,24 +6,15 @@ const Product = ({ product }) => {
 	return (
 		<Card className='my-3 p-3 rounded'>
 			<a href={`/product/${product._id}`}>
-				<Card.Img
-					src={product.image}
-					variant='top'
-				></Card.Img>
+				<Card.Img src={product.image} variant='top'></Card.Img>
 			</a>
 			<Card.Body>
-				<a href={`product/${product._id}`}>
-					<Card.Title
-						as='div'
-						className='product-title'
-					>
+				<a href={`/product/${product._id}`}>
+					<Card.Title as='div' className='product-title'>
 						{product.name}
 					</Card.Title>
 				</a>
-				<Rating
-					value={product.rating}
-					text={`${product.numReviews} reviews`}
-				/>
+				<Rating value={product.rating} text={`${product.numReviews} reviews`} />
 				<Card.Text as='h3'>${product.price}</Card.Text>
 			</Card.Body>
 		</Card>
